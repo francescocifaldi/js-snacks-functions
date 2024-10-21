047639d4bb107ecd27527c30176b5a073852945a
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-21 15:47:15 +0200

    ADD 5th excercise

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function greet(name){
+    let greeting = ''
+    let now = new Date().getHours()
+    
+    if (now < 13){
+        greeting = `buongiorno ${name}`  
+    }
+    else if (now < 17){
+        greeting = `buon pomeriggio ${name}`
+    } else if (now > 17){
+        greeting = `buonasera ${name}`
+    }
+    return greeting;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(greet(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
