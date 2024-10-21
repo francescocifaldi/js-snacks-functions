9378a70e9eb628fd9422a086822c55d468c74afd
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-21 15:31:49 +0200

    ADD 3rd excercise

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,18 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function wovewlCount(word){
+    let count = 0;
+    for(let i=0; i<word.length; i++){
+        if(word.charAt(i)==='a'||word.charAt(i)==='e'||word.charAt(i)==='i'||word.charAt(i)==='o'||word.charAt(i)==='u'){
+            count ++
+        }
+    }
+    return count
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(wovewlCount(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
